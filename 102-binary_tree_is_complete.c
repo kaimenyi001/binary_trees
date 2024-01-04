c202ec0920bc9e99ac1248f55421592d2b151f02
kaimenyi001 <deniskaimenyi1@gmail.com>
2024-01-03 22:03:48 -0800

    levelorder

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,123 @@
+#include "binary_trees.h"
+/**
+ * new_node - Function that creates a new node in a linked_list
+ * @node: node to be created
+ * Return: node created
+ */
+
+link_t *new_node(binary_tree_t *node)
+{
+	link_t *new;
+
+	new =  malloc(sizeof(link_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->node = node;
+	new->next = NULL;
+
+	return (new);
+}
+
+/**
+ * free_q - Function that frees the nodes at the linked list
+ * @head: Node of the linked_list
+ */
+
+void free_q(link_t *head)
+{
+	link_t *temp_node;
+
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
+}
+
+/**
+ * _push - Function that pushes a node into the stack
+ * @node: node of the tree
+ * @head: node of the stack
+ * @tail: tail node of the stack
+ */
+
+void _push(binary_tree_t *node, link_t *head, link_t **tail)
+{
+	link_t *new;
+
+	new = new_node(node);
+	if (new == NULL)
+	{
+		free_q(head);
+		exit(1);
+	}
+	(*tail)->next = new;
+	*tail = new;
+}
+
+/**
+ * _pop - Function that pops a node into the stack
+ * @head: Type head node of in the stack
+ */
+
+void _pop(link_t **head)
+{
+	link_t *temp_node;
+
+	temp_node = (*head)->next;
+	free(*head);
+	*head = temp_node;
+}
+
+/**
+ * binary_tree_is_complete - Function that checks if a binary tree is complete
+ * @tree: root node of the tree
+ * Return: If the tree is NULL or not complete, 0
+ *         Otherwise, 1.
+ */
+
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	link_t *head, *tail;
+	int flag = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	head = tail = new_node((binary_tree_t *)tree);
+	if (head == NULL)
+	{
+		exit(1);
+	}
+	while (head != NULL)
+	{
+		if (head->node->left != NULL)
+		{
+			if (flag == 1)
+			{
+				free_q(head);
+				return (0);
+			}
+			_push(head->node->left, head, &tail);
+		}
+		else
+			flag = 1;
+		if (head->node->right != NULL)
+		{
+			if (flag == 1)
+			{
+				free_q(head);
+				return (0);
+			}
+			_push(head->node->right, head, &tail);
+		}
+		else
+			flag = 1;
+		_pop(&head);
+	}
+	return (1);
+}
